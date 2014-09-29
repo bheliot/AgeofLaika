66700bc1182aa038801f1d7133e2f6a3c62609ab
Belinda Heliot <Belinda@Belindas-MacBook-Air.local>
2014-09-29 18:12:10 -0500

    Find out a dog's age in dog years

---
+++ ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  AgeofLaika
+//
+//  Created by Belinda Heliot on 9/29/14.
+//  Copyright (c) 2014 Belinda Heliot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ageConvertedToDogYearsLabel: UILabel!
+    @IBOutlet weak var enterAgeInHumanYearsTextField: UITextField!
+    @IBAction func convertAgeToDogYearsButtonPressed(sender: UIButton) {
+        ageConvertedToDogYearsLabel.hidden = false
+        enterAgeInHumanYearsTextField.resignFirstResponder()
+        
+        let ageFromTextField = enterAgeInHumanYearsTextField.text.toInt()!
+        let conversionConstant = 7
+        
+       ageConvertedToDogYearsLabel.text =
+            "\(ageFromTextField * conversionConstant)" + " in dog years"
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
